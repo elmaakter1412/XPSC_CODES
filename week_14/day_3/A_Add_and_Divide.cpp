221af05c00f1b0e65828a1dddf94b3aa432c082a
Elma Akter <elmaakter1412@gmail.com>
2023-09-10 23:12:12 +0600

    day_3

---
+++ week_14/day_3/A_Add_and_Divide.cpp
@@ -0,0 +1,54 @@
+//Author: elmaakter14120;
+    
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+    
+void result(){
+    int x,y;
+    cin>>x>>y;
+    int ans,f=0;
+    if(y==1){
+        ans=1;
+        y=2;
+        f++;
+    }
+    else{
+       ans=0;
+       int min=INT_MAX;
+       
+       for(int i=y;i>0;i++){
+        int s = x;
+        while(s>0){
+            s/=y;
+            ans++;
+            
+        }
+        if(min >= ans){
+            min = ans;
+        }
+         else{
+            break;
+        }
+        f++;
+        ans=f;
+        y++;
+         
+    }
+    cout<<min<<"\n";
+    }
+    
+}
+    
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    ll ts_case;
+    cin >> ts_case;
+    
+    while(ts_case--){
+        result();
+    }
+    return 0;
+}
